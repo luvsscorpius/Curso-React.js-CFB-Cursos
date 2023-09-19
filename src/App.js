51e691a5b7c7d20891c88b104f6a975e3bc982ef
and <andersonvittor1801@gmail.com>
2023-09-19 17:59:25 -0300

    feat(aula 06): enviar propriedades para os componentes com PROPS

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default function App() {
       {/* Inserindo componentes */}
       <Header />
       <Body />
+
     </>
   )
 }
--- src/componentes/body.js
+++ src/componentes/body.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Dados from './Dados'
 
 export default function Body() {
+    const canal = 'CFB Cursos'
+    const yt = 'youtube.com/cfbcursos'
+    const crs = 'Curso de React'
     return (
         <section>
             <h2>Curso de React</h2>
             <p>CFB - Cursos</p>
+            <Dados canal={canal} youtube={yt} curso={crs} />
         </section>
     )
 }
