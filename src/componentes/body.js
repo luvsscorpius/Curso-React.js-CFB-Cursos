f9fc1e02a52818be5d98ae26a6462a7788d1bb21
and <andersonvittor1801@gmail.com>
2023-09-20 18:26:41 -0300

    feat(aula 07): enviar funções para componentes React

--- src/componentes/body.js
+++ src/componentes/body.js
@@ -2,14 +2,22 @@ import React from 'react'
 import Dados from './Dados'
 
 export default function Body() {
-    const canal = 'CFB Cursos'
+    const canal = () => {
+        return 'CFB Cursos'
+    }
+
     const yt = 'youtube.com/cfbcursos'
     const crs = 'Curso de React'
+
+    const somar = (v1, v2) => {
+        return v1 + v2
+    }
+
     return (
         <section>
             <h2>Curso de React</h2>
             <p>CFB - Cursos</p>
-            <Dados canal={canal} youtube={yt} curso={crs} />
+            <Dados canal={canal} youtube={yt} curso={crs} somar={somar} />
         </section>
     )
 }
