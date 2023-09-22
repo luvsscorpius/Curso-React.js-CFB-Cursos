c47cb29a84e33e781cf27aadcc9b995a2d9b473a
and <andersonvittor1801@gmail.com>
2023-09-22 09:41:34 -0300

    feat(aula 08): usando CSS em React

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import Header from './componentes/header'
-import Body from './componentes/body'
+import './App.css'
 
 export default function App() {
+
+  const textoDestaque = {
+    color: '#00f',
+    fontSize: '3em'
+  }
+
   return (
     <>
-      {/* Inserindo componentes */}
-      <Header />
-      <Body />
-
+      <section className='caixa'>
+        {/* CSS inline */}
+        <h1 style={{ color: '#f00', fontSize: '5em' }}>CFB Cursos</h1>
+        {/* CSS por constante */}
+        <h2 style={textoDestaque}>Curso de React</h2>
+        <p className='texto'>Se inscreva em nosso canal e nos siga no instagram</p>
+        <a href='https://www.youtube.com/cfbcursos' target='_blank'>CFB Cursos</a>
+      </section>
     </>
   )
 }
