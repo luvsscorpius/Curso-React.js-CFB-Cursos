24b68826d1ccc06471e564a0dceb831132c79703
and <andersonvittor1801@gmail.com>
2023-09-25 18:25:44 -0300

    feat(aula 09): como funciona a renderização de componentes em React

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Relogio from './componentes/Relogio'
 
 export default function App() {
 
@@ -11,12 +12,13 @@ export default function App() {
   return (
     <>
       <section className='caixa'>
+        <Relogio />
         {/* CSS inline */}
         <h1 style={{ color: '#f00', fontSize: '5em' }}>CFB Cursos</h1>
         {/* CSS por constante */}
         <h2 style={textoDestaque}>Curso de React</h2>
         <p className='texto'>Se inscreva em nosso canal e nos siga no instagram</p>
-        <a href='https://www.youtube.com/cfbcursos' target='_blank'>CFB Cursos</a>
+        <a href='https://www.youtube.com/cfbcursos' target='_blank' rel='noopener'>CFB Cursos</a>
       </section>
     </>
   )
