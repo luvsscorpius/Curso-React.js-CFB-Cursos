4619be6ad594bc858ed9155902a3c6f42ceb95ac
and <andersonvittor1801@gmail.com>
2023-12-02 14:56:15 -0300

    feat(aula 14): manipulando elementos de formulário
    Checkbox

--- aula-14-manipulando-elementos-de-formulario/src/App.js
+++ aula-14-manipulando-elementos-de-formulario/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import InputControlado from "./components/Input-controlado/Input-controlado";
+import CheckBox from "./components/Checkbox/Checkbox";
 
 function App() {
   return (
     <>
-      <InputControlado />
+      {/* <InputControlado /> */}
+      <CheckBox />
     </>
   );
 }
