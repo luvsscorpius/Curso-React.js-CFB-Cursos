9052bd9c2d98d6baffa82d60eed3643dca9f02cc
and <andersonvittor1801@gmail.com>
2023-12-02 14:37:06 -0300

    feat(aula 14): manipulando elementos de formulário
    controlled components

--- aula-14-manipulando-elementos-de-formulario/src/components/Input-controlado/Input-controlado.jsx
+++ aula-14-manipulando-elementos-de-formulario/src/components/Input-controlado/Input-controlado.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as C from './Styles.jsx'
 
 const InputControlado = () => {
+    const [nome, setNome] = useState('')
+
+    const handleChange = (event) => {
+        setNome(event.target.value)
+    }
+
     return (
         <>
             <C.Container>
                 <C.Header>
                     <C.h1>Formulário</C.h1>
                 </C.Header>
+
+                <C.Body>
+                    <C.Form>
+                        <C.Label>Nome</C.Label>
+                        <C.Input value={nome} onChange={handleChange} />
+                    </C.Form>
+
+                    <C.Label>Olá {nome}</C.Label>
+                </C.Body>
             </C.Container>
         </>
     )
--- aula-14-manipulando-elementos-de-formulario/src/components/Input-controlado/Styles.jsx
+++ aula-14-manipulando-elementos-de-formulario/src/components/Input-controlado/Styles.jsx
@@ -5,7 +5,8 @@ export const Container = styled.div`
     margin: 20px auto;
     height: 100vh;
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 `;
 
 export const Header = styled.div`
@@ -22,4 +23,31 @@ export const Header = styled.div`
 
 export const h1 = styled.h1``
 
-export const label = styled.label``;
+export const Label = styled.label`
+    margin: 10px;
+    font-size: 1.3rem;
+    font-weight: bold;
+`;
+
+export const Body = styled.div`
+    max-width: 1120px;
+    width: 90%;
+    height: 200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #ddd;
+    border-radius: 2px;
+    margin-top: 10px;
+    gap: 30px;
+`;
+
+export const Form = styled.form``;
+
+export const Input = styled.input`
+    border-radius: 5px;
+    padding: 10px;
+    font-size: 1rem;
+    width: 50%;
+`;
