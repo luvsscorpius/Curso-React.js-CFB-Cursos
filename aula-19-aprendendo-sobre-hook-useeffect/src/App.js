0fac8f78d1ec1d86f31768d1d11e37d12b0327ce
and <andersonvittor1801@gmail.com>
2024-01-29 16:44:04 -0300

    feat(aula 19): aprendendo sobre hook useEffect - exemplos

--- aula-19-aprendendo-sobre-hook-useeffect/src/App.js
+++ aula-19-aprendendo-sobre-hook-useeffect/src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Aula from "./components/Aula/Aula";
+import Counter from "./components/Counter/Counter";
+import BlogPosts from "./components/BlogPosts/BlogPosts";
 
 function App() {
   return (
     <>
-      <Aula/>
+      {/* <Aula/> */}
+      {/* Counter */}
+      {/* <Counter/> */}
+
+      {/* BlogPosts */}
+      <BlogPosts/>
     </>
   );
 }
