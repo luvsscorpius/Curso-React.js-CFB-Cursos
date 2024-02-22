9e53ecfdf79ec0716508e467f5dae4e13f57425e
and <andersonvittor1801@gmail.com>
2024-02-22 18:24:57 -0300

    feat(aula 21): componentes de classe - aula

---
+++ aula-21-componentes-de-classe/src/Components/Aula/Aula.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Classe from './Classe'
+
+const Aula = () => {
+    return (
+        <>
+            <h1>Componentes de Classe</h1>
+            <Classe canal="CFB Cursos" curso="Curso de React"/>
+        </>
+    )
+}
+
+export default Aula
