bd739daadc01979f49fc43abb21582ccbdac097e
and <andersonvittor1801@gmail.com>
2024-02-23 15:59:04 -0300

    feat(aula 21): exercicios - counter

--- aula-21-componentes-de-classe/src/App.js
+++ aula-21-componentes-de-classe/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Aula from './Components/Aula/Aula';
 import Welcome from './Components/Welcome/Welcome';
+import Counter from './Components/Counter/Counter';
 
 function App() {
   return (
     <>
       {/* <Aula/> */}
 
-      <Welcome/>
+      {/* <Welcome/> */}
+
+      <Counter/>
     </>
   );
 }
