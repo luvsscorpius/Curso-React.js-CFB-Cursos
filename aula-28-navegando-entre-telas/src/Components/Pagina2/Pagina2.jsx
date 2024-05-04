a6af3147d27ea4e995ba20ad7ea372a31d397439
Anderson Vitor <andersonvittor1801@gmail.com>
2024-05-04 18:48:33 -0300

    feat(aula 28): navegando entre as telas

--- aula-28-navegando-entre-telas/src/Components/Pagina2/Pagina2.jsx
+++ aula-28-navegando-entre-telas/src/Components/Pagina2/Pagina2.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Pagina2() {
-  return (
-    <div>
-        <h1>Página 2</h1>
-        <h3>Canal CFB Cursos</h3>
-    </div>
-  );
+export default function Pagina1() {
+    return (
+        <div>
+            <h1>Pagina 1</h1>
+            <h3>Curso de React</h3>
+        </div>
+    )
 }
